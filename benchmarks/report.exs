7a60ed77b8d55e098602a7d719b546fe0ff2be5b
Tobias Pfeiffer <pragtob@gmail.com>
2023-12-21 11:29:39 +0100

    run all the formatters

--- benchmarks/report.exs
+++ benchmarks/report.exs
@@ -1,9 +1,8 @@
 Benchee.report(
   load: ["benchmarks/saves_old_benchee/tco_*.benchee"],
   formatters: [
-    # {Benchee.Formatters.Console, extended_statistics: true},
+    {Benchee.Formatters.Console, extended_statistics: true},
     {Benchee.Formatters.HTML,
      file: "benchmarks/output/tco_focussed_detailed_inputs.html", auto_open: false}
-    # fn suite -> Process.sleep(5000);Benchee.Formatters.HTML.sequential_output(suite, %{file: "benchmarks/output/tco_focussed_detailed_inputs.html", auto_open: false}) end
   ]
 )
